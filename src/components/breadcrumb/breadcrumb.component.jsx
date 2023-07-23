9912f0902684ad3bab3998852e079bf6524580ee
Krupesh Bhosle <bhoslekrupesh03@gmail.com>
2023-07-23 20:17:03 +0530

    breadcrumb styled

--- src/components/breadcrumb/breadcrumb.component.jsx
+++ src/components/breadcrumb/breadcrumb.component.jsx
@@ -6,20 +6,20 @@ import { Link } from 'react-router-dom';
 const BreadCrumb = ({ paths }) => {
     return (
         <div className='breadcrumb__component'>
-            {
-                paths.map( (path, key) => (
-                    <div className='path' key={key}> 
-                        { 
-                            path.name === 'home' ? 
-                                <HomeIcon /> 
-                            : 
-                                <div className='breadcrumb-path'>
-                                    &nbsp;&gt; <Link className='breadcrumb-link' to={path.url}> {path.name} </Link>
-                                </div>
-                        } 
-                    </div>
-                ))
-            }
+            <ul class="breadcrumb">
+                {
+                    paths.map( (path, key) => (
+                        <li key={key}>
+                            { 
+                                path.name === 'home' ? 
+                                    <HomeIcon sx={{ height: 20, width: 20 }} />
+                                : 
+                                    <Link to={path.url}>{path.name}</Link>
+                            } 
+                        </li>
+                    ))
+                }
+            </ul>
         </div>
     );
 }
